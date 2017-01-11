3bb56ea7b51a8e442c17603a5e73a7ed222f54ed
alistair.skinner <alistair@thepetspyjamas.com>
2017-01-11 17:12:11 +0000

    added basic error handling for get_bbc_data

--- leto_test/movies/api.py
+++ leto_test/movies/api.py
@@ -4,14 +4,10 @@ import requests
 BBC_URL = 'http://www.bbc.co.uk/tv/programmes/formats/films/player/episodes.json'
 
 def get_bbc_data():
+    # check cache for movies
     movies = cache.get('movies', None)
-    if movies:
-        print 'movies from cache'
-    else:
-        print 'no movies in cache'
     if not movies:
-        print 'getting movies'
         movies = requests.get(BBC_URL).json()
-        cache.set('movies', movies, 5)
-        print 'movies cached'
+        # cache movies for 5 mins
+        cache.set('movies', movies, 300)
     return movies
--- leto_test/movies/views.py
+++ leto_test/movies/views.py
@@ -1,13 +1,17 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 from movies.api import get_bbc_data
 
 class MovieList(TemplateView):
-    
+
     template_name = 'movies.html'
 
     def get_context_data(self, **kwargs):
         context = super(MovieList, self).get_context_data(**kwargs)
-        movies = get_bbc_data()
-        context['movies'] = movies.get('episodes')
+        try:
+            movies = get_bbc_data()
+            context['movies'] = movies.get('episodes')
+        except:
+            messages.error(self.request, 'Error getting data from BBC')
         return context
