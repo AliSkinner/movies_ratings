4e11862ffbf84f95c1a36d55aaa34f6797715fcb
alistair.skinner <alistair@thepetspyjamas.com>
2017-01-11 16:36:28 +0000

    setup base template & wired up basic home view.

--- leto_test/movies/views.py
+++ leto_test/movies/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class MovieList(TemplateView):
+    template_name = 'movies.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieList, self).get_context_data(**kwargs)
+        context['test'] = 'hi'
+        return context
