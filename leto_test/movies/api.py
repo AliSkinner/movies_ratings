107ff19f0a3a978c8901811e40561c74c3a04677
alistair.skinner <alistair@thepetspyjamas.com>
2017-01-12 21:37:38 +0000

    added release date to movie component

--- leto_test/movies/api.py
+++ leto_test/movies/api.py
@@ -1,8 +1,13 @@
 """Module to handle external API calls and cleaning of data"""
 
-import time
-from leto_test.settings import THE_MOVIE_DB_KEY
+import datetime
+import logging
 import requests
+import time
+
+from leto_test.settings import THE_MOVIE_DB_KEY
+
+logger = logging.getLogger(__name__)
 
 BBC_URL = "http://www.bbc.co.uk/tv/programmes/formats/films/player/episodes.json"
 THE_MOVIE_DB_URL = "http://api.themoviedb.org/3/search/movie?api_key={}"\
@@ -29,6 +34,7 @@ def get_movie_details(movie):
         'poster': "http://ichef.bbci.co.uk/images/ic/480x270/{}.jpg"
                   .format(movie.get('image').get('pid')),
         'rating': 'Not Rated',
+        'release_date': "Unkown"
     }
 
     search_data = requests.get("{}&query={}"
@@ -41,5 +47,13 @@ def get_movie_details(movie):
     if search_data.get('total_results') > 0:
         movie_data = search_data.get('results')[0]
         movie_details['rating'] = movie_data.get('vote_average')
+        try:
+            movie_details['release_date'] = datetime.datetime.strptime(
+                movie_data.get('release_date'),
+                '%Y-%m-%d'
+            )
+        except Exception as e:
+            logger.debug(e)
+            pass
 
     return movie_details
