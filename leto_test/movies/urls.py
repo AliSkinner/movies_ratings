244b5e5d76a74e29807ae1ccf31f2cc28612ecf3
alistair.skinner <alistair@thepetspyjamas.com>
2017-01-13 12:40:21 +0000

    removed unused refs to models & admin

--- leto_test/movies/urls.py
+++ leto_test/movies/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.MovieList.as_view(), name="movies")
+    url(r'^', views.MovieList.as_view(), name='movies')
 ]
